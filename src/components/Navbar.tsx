c0bc6e92681c6aa5c875f54b00437442228972b6
Pedro Pinto <pmap@ua.pt>
2024-04-16 11:34:34 +0100

    small changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ function Navbar() {
     return ( 
         // <!-- Navbar -->
         <nav className="bg-white border-gray-200 px-4 py-2">
-            <div className="max-w-screen-xl mx-auto flex items-center">
+            <div className="max-w-screen-xl mx-auto flex items-center justify-between">
                 {/* Brand/Logo Area */}
-                <a href="#" className="text-2xl font-semibold flex-1">CalendarSync UA</a>
+                <a href="#" className="text-2xl font-semibold">CalendarSync UA</a>
                 
                 {/* Menu for larger screens, moved towards the left */}
-                <div className="hidden md:flex justify-start items-center space-x-4 flex-1">
+                <div className="hidden md:flex justify-center items-center space-x-4">
                 <a href="#" className="text-blue-700 hover:text-blue-900">Calendar</a>
                 <a href="#" className="text-gray-900 hover:text-blue-700">About</a>
                 </div>
