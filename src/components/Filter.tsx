db0efdf77b72150fbc3f87cb399fff88ed1966a7
Pedro Pinto <pmap@ua.pt>
2024-04-16 13:43:17 +0100

    done

---
+++ src/components/Filter.tsx
@@ -0,0 +1,17 @@
+function Filter() {
+    return ( 
+        // <!-- Main  -->
+        <>
+        <div className="border-solid bg-slate-200 border-2 border-sky-600 rounded">
+            <div className="py-5 px-5 flex justify-center border-solid border-b-2 border-sky-600"> 
+                <h1 className="">Filter</h1>
+            </div>
+            <div> 
+                <h1 className=""></h1>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Filter;
