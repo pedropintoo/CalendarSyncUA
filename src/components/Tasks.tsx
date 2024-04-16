368a6ca706e8d61b7797d3148d90c90ac330b0cd
Pedro Pinto <pmap@ua.pt>
2024-04-16 15:21:22 +0100

    basic shape

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,17 @@
+function Tasks() {
+    return ( 
+        // <!-- Main  -->
+        <>
+        <div className="border-solid bg-slate-200 border-2 border-sky-600 rounded">
+            <div className="py-5 px-5 flex justify-center border-solid border-b-2 border-sky-600"> 
+                <h1 className="">Tasks</h1>
+            </div>
+            <div> 
+                <h1 className=""></h1>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Tasks;
