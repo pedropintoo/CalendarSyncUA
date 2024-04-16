18b96e6c8f530e4f30af402354bac4d7a434a738
Pedro Pinto <pmap@ua.pt>
2024-04-16 16:26:03 +0100

    fix sizes compatibility

--- src/components/MainStructure.tsx
+++ src/components/MainStructure.tsx
@@ -5,7 +5,7 @@ function MainStructure() {
     return ( 
         // <!-- Main  -->
         <>
-        <div className="px-4 py-4 mx-auto grid lg:grid-cols-6 gap-2 md:h-screen">
+        <div className="px-4 py-4 mx-auto grid lg:grid-cols-6 gap-2 min-h-screen">
             <Filter/>
             <div className="bg-gray-200 lg:col-span-4 rounded-lg"></div>
             <Tasks/>
