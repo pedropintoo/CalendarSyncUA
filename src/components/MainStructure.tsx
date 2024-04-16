3cf9bb28a64dc7743397a832ca6499124a6afadf
Pedro Pinto <pmap@ua.pt>
2024-04-16 13:17:49 +0100

    Main Structure

--- src/components/MainStructure.tsx
+++ src/components/MainStructure.tsx
@@ -2,11 +2,11 @@ function MainStructure() {
     return ( 
         // <!-- Main  -->
         <>
-            <div className="px-4 py-4 mx-auto grid md:grid-cols-6 gap-2 md:h-screen">
-                <div className="bg-blue-300 rounded-lg"></div>
-                <div className="bg-gray-200 md:col-span-4 rounded-lg"></div>
-                <div className="bg-blue-300 rounded-lg"></div>
-            </div>
+        <div className="px-4 py-4 mx-auto grid md:grid-cols-6 gap-2 md:h-screen">
+            <div className="bg-blue-300 rounded-lg"></div>
+            <div className="bg-gray-200 md:col-span-4 rounded-lg"></div>
+            <div className="bg-blue-300 rounded-lg"></div>
+        </div>
         </>
      );
 }
