152ec65f10b890b20c8257951f7ed2556aee386a
Pedro Pinto <pmap@ua.pt>
2024-04-16 15:55:36 +0100

    fix navbar size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,33 @@
 function Navbar() {
+    const calendar = <a href="#" className="block md:flex text-blue-700 hover:text-sky-600">Calendar</a>;
+    const about = <a href="#" className="block md:flex text-gray-900 hover:text-sky-600">About</a>;
+    const getStarted = <a href="#" className="flex justify-end text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2">Get started</a>
+    const name = <a href="#" className="text-2xl md:flex justify-center md:col-span-4 font-semibold">CalendarSync UA</a>;
+    
     return ( 
         // <!-- Navbar -->
-        <nav className="bg-white border-gray-200 px-4 py-2">
-            <div className="max-w-screen-xl mx-auto flex items-center justify-between">
-                {/* Brand/Logo Area */}
-                <a href="#" className="text-2xl font-semibold">CalendarSync UA</a>
+        <nav className="px-4 py-3 shadow bg-white border-gray-200">
+            <div className="mx-auto grid md:grid-cols-6 gap-0">
                 
                 {/* Menu for larger screens, moved towards the left */}
-                <div className="hidden md:flex justify-center items-center space-x-4">
-                <a href="#" className="text-blue-700 hover:text-blue-900">Calendar</a>
-                <a href="#" className="text-gray-900 hover:text-blue-700">About</a>
+                <div className="hidden md:flex justify-start items-center space-x-4">
+                    {calendar}
+                    {about}
                 </div>
+
+                {/* Brand/Logo Area */}
+                {name}
                 
                 {/* Get Started Button - Adjusted to be on the far right */}
-                <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Get started</a>
+                <div className="hidden md:grid justify-end">
+                    {getStarted}
+                </div>
             </div>
             
             {/* Simplified Mobile Menu - Initially hidden */}
             <div className="md:hidden">
-                <a href="#" className="block text-blue-700 hover:text-blue-900">Calendar</a>
-                <a href="#" className="block text-gray-900 hover:text-blue-700">About</a>
+                {calendar}
+                {about}
             </div>
         </nav>
 
