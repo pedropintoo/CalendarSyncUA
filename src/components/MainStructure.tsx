70b9a44274223280c56a9fc21bcb7a687fcc4553
Pedro Pinto <120741472+pedropintoo@users.noreply.github.com>
2024-04-16 13:02:28 +0100

    Revert "Feature/tasks"

--- src/components/MainStructure.tsx
+++ src/components/MainStructure.tsx
@@ -2,10 +2,10 @@ function MainStructure() {
     return ( 
         // <!-- Main -->
         <>
-            <div className="px-4 py-4 mx-auto grid md:grid-cols-6 gap-2 md:h-screen">
-                <div className="bg-blue-300 rounded-lg"></div>
-                <div className="bg-gray-200 md:col-span-4 rounded-lg"></div>
-                <div className="bg-blue-300 rounded-lg"></div>
+            <div className="px-4 py-2 mx-auto grid md:grid-cols-6 gap-2">
+                <div className="bg-red-300">left side</div>
+                <div className="bg-green-200 md:col-span-4 pl-2">main</div>
+                <div className="bg-red-300">right side</div>
             </div>
         </>
      );
