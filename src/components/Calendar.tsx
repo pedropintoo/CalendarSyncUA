f6d1d55ca478ef99b4089d9ec415a8b23dafad59
Guilherme Santos <gui.santos91@ua.pt>
2024-04-17 09:20:18 +0100

    Calendar component with border

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+function Calendar() {
+    return ( 
+        // <!-- Main  -->
+        <>
+        <div className="border-solid bg-slate-200 bg-gray-200 lg:col-span-4 rounded-lg border-2 border-sky-600 rounded">
+            <div> 
+                <h1 className=""></h1>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Calendar;
