74895c14fad4a9d0196d2cb80e5fbff8a34542cd
Pedro Pinto <pmap@ua.pt>
2024-05-03 23:39:58 +0100

    DEMO

--- src/components/MainStructure.tsx
+++ src/components/MainStructure.tsx
@@ -4,6 +4,7 @@ import Filter from "./Filter";
 import Tasks from "./Tasks";
 
 interface EventICSProps {
+    id: string;
     title: string;
     startDate: Date;
     endDate: Date;
@@ -22,6 +23,26 @@ function MainStructure() {
         setAllEventsICS([...allEventsICS, newEvent]);
     };
 
+    // ONLY DEMO!!!
+    const addDEMOData = () => {
+        console.log("Adding demo data")
+        // RAW DATA ONLY FOR DEMO
+        const rawData = `BEGIN:VCALENDAR
+        VERSION:2.0
+        PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+        BEGIN:VEVENT
+        UID:
+        DTSTAMP:20210901T000000Z
+        DTSTART:20210901T100000Z
+        DTEND:20210901T120000Z
+        SUMMARY:Meeting
+        DESCRIPTION:Meeting with the team
+        LOCATION:Office
+        END:VEVENT
+        END:VCALENDAR`;
+        handleAddEvent({id: "1", title: "Meeting", startDate: new Date("2021-09-01T10:00:00Z"), endDate: new Date("2021-09-01T12:00:00Z"), description: "Meeting with the team", location: "Office", rawData: rawData});
+    }
+    
     return ( 
         // <!-- Main  -->
         <>
@@ -30,6 +51,7 @@ function MainStructure() {
             <Calendar allEvents={allEventsICS} filteredEvents={filteredEventsICS} setAllEvents={setAllEventsICS} />
             <Tasks allEvents={filteredEventsICS} />
         </div>
+        <button onClick={addDEMOData} className="btn-square bg-sky-900 text-slate-50 w-100">Demo data</button>
         </>
      );
 }
