933a7d7b0d39e41ef4b83970a9ccc3983f05849b
Pedro Pinto <pmap@ua.pt>
2024-05-03 23:22:59 +0100

    small fix

--- src/components/MainStructure.tsx
+++ src/components/MainStructure.tsx
@@ -1,15 +1,34 @@
 import Calendar from "./Calendar";
+import React, { useState } from 'react';
 import Filter from "./Filter";
 import Tasks from "./Tasks";
 
+interface EventICSProps {
+    title: string;
+    startDate: Date;
+    endDate: Date;
+    description?: string;
+    location?: string;
+    rawData: string; // raw ics data
+  }
+
 function MainStructure() {
+    
+    const [allEventsICS, setAllEventsICS] = useState<EventICSProps[]>([]);
+    const [filteredEventsICS, setFilteredEventsICS] = useState<EventICSProps[]>([]);
+
+    // Function to add a new event
+    const handleAddEvent = (newEvent: EventICSProps) => {
+        setAllEventsICS([...allEventsICS, newEvent]);
+    };
+
     return ( 
         // <!-- Main  -->
         <>
         <div className="px-4 py-4 mx-auto grid lg:grid-cols-6 gap-2 min-h-screen">
-            <Filter/>
-            <Calendar/>
-            <Tasks/>
+            <Filter allEvents={allEventsICS} setFilteredEvents={setFilteredEventsICS} />
+            <Calendar allEvents={allEventsICS} filteredEvents={filteredEventsICS} setAllEvents={setAllEventsICS} />
+            <Tasks allEvents={filteredEventsICS} />
         </div>
         </>
      );
