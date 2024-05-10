981a163167a7ca7770287aa8d652ae607ce4d863
Guilherme Santos <gui.santos91@ua.pt>
2024-05-10 15:17:09 +0100

    addevent

--- src/components/calendar/AddEventModal.tsx
+++ src/components/calendar/AddEventModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useStructureContext } from '../contexts/StructureContext';
-import { CalendarContext, useCalendarContext } from '../contexts/CalendarContext';
+import { useCalendarContext } from '../contexts/CalendarContext';
 
 interface AddEventForm {
   title: string;
@@ -29,6 +29,10 @@ function AddEventModal(){
       setForm({...form, [event.target.name]: event.target.value });
     };
 
+    const handleClose = () =>{
+      CC.setAddEventOpen(false);
+    }
+
 
     const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
@@ -55,42 +59,68 @@ function AddEventModal(){
     return (
         <>
             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-              <form onSubmit={handleFormSubmit}>
-                <label>
-                  Title:
-                  <input type="text" name="title" value={form.title} onChange={handleFormChange} />
-                </label>
-                <label>
-                  Description:
-                  <textarea name="description" value={form.description} onChange={handleFormChange} />
-                </label>
-                <label>
-                  Start Date:
-                  <input type="date" name="startDate" value={form.startDate} onChange={handleFormChange} />
-                </label>
-                <label>
-                  Start Hour:
-                  <input type="time" name="startHour" value={form.startHour} onChange={handleFormChange} />
-                </label>
-                <label>
-                  End Date:
-                  <input type="date" name="endDate" value={form.endDate} onChange={handleFormChange} />
-                </label>
-                <label>
-                  End Hour:
-                  <input type="time" name="endHour" value={form.endHour} onChange={handleFormChange} />
-                </label>
-                <label>
-                  Tag:
-                  <select name="tag" value={form.tag} onChange={handleFormChange}>
-                      <option value="">Select a tag</option>
-                      {Object.entries(SC.tags).map(([tagName, colorClass]) => (
-                        <option key={tagName} value={tagName} className="mr-2 ml-3" />
-                      ))}
-                  </select>
-                </label>
-                <button type="submit">Confirm</button>
-              </form>
+              <div className='relative p-4 w-full max-w-md max-h-full bg-white p-8 rounded-lg shadow-lg'>
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                  <h3 className="text-lg">
+                      Add Event
+                  </h3>
+                  <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={handleClose}>
+                      <svg className="w-3 h-3" aria-hidden="true" fill="none" viewBox="0 0 14 14">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                      </svg>
+                      <span className="sr-only">Close modal</span>
+                  </button>
+              </div>
+                <form onSubmit={handleFormSubmit} className='p-4 md:p-5'>
+                    <div className='grid gap-4 mb-4 grid-cols-1'>
+                      <label htmlFor='title' className='block mb-2 text-bg font-medium'>Title:</label>
+                      <input type="text" name="title" id='title' value={form.title} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' placeholder='Write here' required/>
+                      
+                      <label htmlFor='description' className='block mb-2 text-bg font-medium'>Description:</label>
+                      <textarea name="description" id='description' value={form.description} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' placeholder='Write here'/>
+                    </div>
+                  <div className='grid gap-4 mb-4 grid-cols-2'>
+                    <label className='block mb-2 text-bg font-medium'>
+                      Start Date:
+                      <input type="date" name="startDate" value={form.startDate} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' required/>
+                    </label>
+                    <label className='block mb-2 text-bg font-medium'>
+                      End Date:
+                      <input type="date" name="endDate" value={form.endDate} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' required/>
+                    </label>
+                    <label className='block mb-2 text-bg font-medium'>
+                      Start Hour:
+                      <input type="time" name="startHour" value={form.startHour} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' required/>
+                    </label>
+                    <label className='block mb-2 text-bg font-medium'>
+                      End Hour:
+                      <input type="time" name="endHour" value={form.endHour} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' required/>
+                    </label>
+                  </div>
+                  <div className='grid gap-4 mb-10 grid-cols-1'>
+                    <label htmlFor='tag' className='block mb-2 text-bg font-medium'>
+                      Tag:
+                    </label>
+                      <select name="tag" id='tag' value={form.tag} onChange={handleFormChange} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' required>
+                          <option value="">Select a tag</option>
+                          {Object.entries(SC.tags).map(([tagName]) => (
+                            <option key={tagName} value={tagName}>{tagName}</option>
+                          ))}
+                      </select>
+                  </div>
+                  
+                <div className='grid gap-4 mb-4 grid-cols-2'>
+                  <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                      Add Event
+                  </button>
+                  <button type="button" className="text-white inline-flex items-center bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={handleClose}>
+                        Close
+                        <span className="sr-only">Close modal</span>
+                  </button>
+                </div>
+                </form>
+              </div>
             </div>
       </>
     );
