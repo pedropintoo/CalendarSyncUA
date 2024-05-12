031ff9adb620b0aafc978e9458e872dbc5ff1d13
Pedro Pinto <pmap@ua.pt>
2024-05-12 01:33:36 +0100

    done

--- src/components/tasks/TaskEvent.tsx
+++ src/components/tasks/TaskEvent.tsx
@@ -19,7 +19,7 @@ function TaskEvent({handleEvent, event}: {handleEvent: () => void,  event: Event
     };
 
     return (
-        <div className="relative mx-2 my-4 px-1 py-1 rounded cursor-pointer overflow-x-auto whitespace-nowrap" style={{ border: `2px solid ${color}` }} onClick={openEditModal}>
+        <div className="relative mx-2 my-4 px-1 py-1 rounded cursor-pointer overflow-x-auto whitespace-nowrap" onClick={openEditModal}>
             {/* Left color bar */}
             <div className="absolute w-2 rounded-tl-none rounded-bl-none h-full left-0 top-0" style={{backgroundColor: color}}></div>
             {/* Event content with padding only on the right and top/bottom to leave space for the color bar */}
