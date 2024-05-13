af0d5b89e69dc365b12cebc9f737dc3ea620631a
Guilherme Santos <gui.santos91@ua.pt>
2024-05-13 23:46:05 +0100

    commit

--- src/components/tasks/ViewEventModal.tsx
+++ src/components/tasks/ViewEventModal.tsx
@@ -50,14 +50,14 @@ const ViewEventModal = ({ thisEvent, setIsOpen, clickCoordinates, openEdit, setC
     return (
         <>
             <div className="fixed z-50" style={{ top: clickCoordinates.y, left: clickCoordinates.x }}>
-                <div ref={modalRef} className="relative p-6 w-full max-w-md h-full bg-white dark:bg-gray-700 rounded-lg shadow-lg">
+                <div ref={modalRef} className="relative p-6 w-full max-w-md h-full bg-white rounded-lg shadow-lg">
                     <div className="flex items-center justify-between mb-4">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white overflow-x-auto">
+                        <h3 className="text-xl font-semibold text-gray-900 overflow-x-auto">
                             {thisEvent.title}
                         </h3>
                         <button
                             type="button"
-                            className="rounded-lg p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700"
+                            className="rounded-lg p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-800"
                             onClick={(e) => { e.stopPropagation(); closeModal(); }}
                             aria-label="Close"
                         >
@@ -68,7 +68,7 @@ const ViewEventModal = ({ thisEvent, setIsOpen, clickCoordinates, openEdit, setC
                     </div>
                     {thisEvent.description && (
                         <div className="mb-4 overflow-y-auto overflow-x-hidden" style={{ maxHeight: '100px' }}>
-                            <label htmlFor='description' className='block text-sm font-medium text-gray-700 dark:text-gray-300'>Description:</label>
+                            <label htmlFor='description' className='block text-sm font-medium text-gray-700 '>Description:</label>
                             <p className="text-sm text-gray-600 dark:text-gray-400 whitespace-normal">
                                 {thisEvent.description}
                             </p>
@@ -76,33 +76,33 @@ const ViewEventModal = ({ thisEvent, setIsOpen, clickCoordinates, openEdit, setC
                     )}
                     <div className='grid gap-4 mb-4 grid-cols-2 text-sm'>
                         <div>
-                            <h4 className='font-medium text-gray-700 dark:text-gray-300'>Start Date:</h4>
+                            <h4 className='font-medium text-gray-700'>Start Date:</h4>
                             <p className="text-gray-600 dark:text-gray-400">
                                 {adjustedStartDate.toLocaleDateString()}
                             </p>
                         </div>
                         <div>
-                            <h4 className='font-medium text-gray-700 dark:text-gray-300'>End Date:</h4>
+                            <h4 className='font-medium text-gray-700'>End Date:</h4>
                             <p className="text-gray-600 dark:text-gray-400">
                                 {adjustedEndDate.toLocaleDateString()}
                             </p>
                         </div>
                         <div>
-                            <h4 className='font-medium text-gray-700 dark:text-gray-300'>Start Time:</h4>
+                            <h4 className='font-medium text-gray-700'>Start Time:</h4>
                             <p className="text-gray-600 dark:text-gray-400">
                                 {adjustedStartDate.toLocaleTimeString()}
                             </p>
                         </div>
                         <div>
-                            <h4 className='font-medium text-gray-700 dark:text-gray-300'>End Time:</h4>
+                            <h4 className='font-medium text-gray-700'>End Time:</h4>
                             <p className="text-gray-600 dark:text-gray-400">
                                 {adjustedEndDate.toLocaleTimeString()}
                             </p>
                         </div>
                     </div>
                     <div className='mb-10'>
-                        <h4 htmlFor='tag' className='block text-sm font-medium text-gray-700 dark:text-gray-300'>Tag:</h4>
-                        <p className="text-gray-600 dark:text-gray-400">
+                        <h4 className='block text-sm font-medium text-gray-700'>Tag:</h4>
+                        <p className="text-gray-600">
                             {thisEvent.tagName}
                         </p>
                     </div>
